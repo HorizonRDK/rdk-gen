05c8923eecd211a71adbb48accceab28f086f232
yaqiang.li <yaqiang.li@horizon.ai>
2023-04-06 15:45:26 +0800

    fix: Delete the debain package of the old version
    Summary:
        Except for the package of hobot_models_basic, the naming is
        not standardized and cannot be processed.
    
    Signed-off-by: yaqiang.li <yaqiang.li@horizon.ai>

--- download_deb_pkgs.sh
+++ download_deb_pkgs.sh
@@ -4,7 +4,7 @@
  # Copyright 2023 Horizon Robotics, Inc.
  # All rights reserved.
  # @Date: 2023-03-15 15:58:13
- # @LastEditTime: 2023-03-19 13:39:45
+ # @LastEditTime: 2023-04-06 15:38:14
 ### 
 
 set -ex
@@ -23,20 +23,20 @@ main()
         hobot-io hobot-io-samples \
         hobot-multimedia hobot-multimedia-dev hobot-camera hobot-dnn \
         hobot-models-basic hhp-verify tros \
-	hobot-sp-cdev hobot-arm64-srcampy hobot-arm64-dnn-python )
+        hobot-sp-cdev hobot-arm64-srcampy hobot-arm64-dnn-python )
 
     if curl -sfO "${archive_url}/dists/focal/main/binary-arm64/Packages"; then
-	echo "File Packages downloaded successfully"
+        echo "File Packages downloaded successfully"
     else
-	echo "File Packages downloaded failed"
-	return -1
+        echo "File Packages downloaded failed"
+        return -1
     fi
 
     for pkg_name in ${deb_pkg_list[@]}
     do
         # Get the latest version number from the Packages file
         VERSION=$(cat Packages | awk -v pkg=${pkg_name} '$1 == "Package:" && $2 == pkg {getline; print}' | awk '{print $2}' | sort -V | tail -n1)
-	FILENAME=$(grep -A 10 -E "^Package: ${pkg_name}$" Packages | grep '^Filename: ' | cut -d ' ' -f 2 | sort -V | tail -n1)
+        FILENAME=$(grep -A 10 -E "^Package: ${pkg_name}$" Packages | grep '^Filename: ' | cut -d ' ' -f 2 | sort -V | tail -n1)
 
         if [[ -z "$VERSION" ]]; then
             echo "Error: Unable to retrieve version number for $pkg_name" >&2
@@ -48,15 +48,15 @@ main()
 
         # Loop through each file and delete any with a lower version number than the latest version
         for file in $FILES; do
-            file_version=$(echo $file | sed "s/${pkg_name}_\(.*\)_arm64.deb/\1/")
+            file_version=$(echo ${file} | sed "s/${pkg_name}_\(.*\)_arm64.deb/\1/")
             if [[ $file_version < $VERSION ]]; then
-                echo "Deleting older version of $PKG_FILE"
-                # rm "$file"
+                echo "Deleting older version of ${file}"
+                rm "${file}"
             fi
         done
 
         # Construct the name of the deb package
-	PKG_FILE=$(basename "${FILENAME}")
+        PKG_FILE=$(basename "${FILENAME}")
 
         # Check if the package has already been downloaded
         if [[ -f "$PKG_FILE" ]]; then
