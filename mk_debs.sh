712b0e5e1bd64f66112b1a7b9d6deb81947370a1
yaqiang.li <yaqiang.li@horizon.cc>
2023-08-10 15:01:12 +0800

    update(deb):Update hobot-boot and hobot-spdev package version
    Signed-off-by: yaqiang.li <yaqiang.li@horizon.cc>

--- mk_debs.sh
+++ mk_debs.sh
@@ -422,7 +422,7 @@ function make_debian_deb() {
 }
 
 declare -A deb_pkg_version
-deb_pkg_version["hobot-boot"]="2.0.1"
+deb_pkg_version["hobot-boot"]="2.0.2"
 deb_pkg_version["hobot-kernel-headers"]="2.0.0"
 deb_pkg_version["hobot-dtb"]="2.0.0"
 deb_pkg_version["hobot-bpu-drivers"]="2.0.0"
@@ -436,7 +436,7 @@ deb_pkg_version["hobot-multimedia"]="2.0.1"
 deb_pkg_version["hobot-multimedia-dev"]="2.0.0"
 deb_pkg_version["hobot-camera"]="2.1.0"
 deb_pkg_version["hobot-dnn"]="2.1.0"
-deb_pkg_version["hobot-spdev"]="2.1.0"
+deb_pkg_version["hobot-spdev"]="2.1.1"
 deb_pkg_version["hobot-sp-samples"]="2.1.0"
 deb_pkg_version["hobot-multimedia-samples"]="2.0.0"
 
