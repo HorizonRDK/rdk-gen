5c615519e5b774852749a920315ee896a8012ecc
yaqiang.li <yaqiang.li@horizon.cc>
2024-01-02 18:17:18 +0800

    fix(deb): Restore use of ubuntu-rdk
    Signed-off-by: yaqiang.li <yaqiang.li@horizon.cc>

--- download_deb_pkgs.sh
+++ download_deb_pkgs.sh
@@ -7,7 +7,7 @@
  # @LastEditTime: 2023-05-15 14:50:53
 ###
 
-archive_url="http://sunrise.horizon.cc/ubuntu-rdk-beta"
+archive_url="http://sunrise.horizon.cc/ubuntu-rdk"
 
 deb_pkg_list=(hobot-multimedia-samples \
     hobot-sp-samples \
